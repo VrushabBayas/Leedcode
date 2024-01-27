678f509aa704f50797ecff09d35e3cc69e452b02
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-27 10:09:16 +0530

    Add bitwise operators and examples

--- bitwise.js
+++ bitwise.js
@@ -1,15 +1,40 @@
-function findMSB(number) {
-  // Convert the number to binary representation
-  const binaryRepresentation = (number >> 32).toString(2);
-  console.log("binaryRepresentation: ", binaryRepresentation);
+// And operator
+// Bitwise AND operator
+// 0 & 0 = 0
+// 0 & 1 = 0
+// 1 & 0 = 0
+// 1 & 1 = 1
+console.log(10 & 2);
+// Or operator
+// Bitwise OR operator
+// 0 | 0 = 0
+// 0 | 1 = 1
+// 1 | 0 = 1
+// 1 | 1 = 1
+console.log(10 | 2);
+// XOR operator
+// Bitwise XOR operator
+// 0 ^ 0 = 0
+// 0 ^ 1 = 1
+// 1 ^ 0 = 1
+// 1 ^ 1 = 0
+console.log(10 ^ 2);
+// Not operator
+// Bitwise NOT operator
+// ~0 = 1 and ~1 = 0
 
-  // The MSB is the leftmost digit in the binary representation
-  const msb = binaryRepresentation.charAt(0);
+// Left shift operator
+// 00001010 << 2 = 00101000
+console.log(8 << 2);
 
-  return msb;
-}
+// Right shift operator
+// 00001010 >> 2 = 00000010
+console.log(8 >> 2);
 
-// Example usage:
-const number = 7;
-const msb = findMSB(number);
-console.log(`The MSB of ${number} is ${msb}`);
+// Zero fill right shift operator
+
+// 00001010 >>> 2 = 00000010
+console.log(10 >>> 2);
+
+let d = ~1
+d
