c75a5cca9dbe37bedb4d7dd015cb8d0ccb67b673
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-02-02 07:53:33 +0530

    added more problem statement

---
+++ shortestCompletingWord.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} licensePlate
+ * @param {string[]} words
+ * @return {string}
+ */
+const checkSubStr = (str1, str2) => {};
+var shortestCompletingWord = function (licensePlate, words) {
+  let updatedString = licensePlate.replace(/[^a-zA-z]/g, "");
+  let result = words.filter((word) => {});
+  return updatedString;
+};
+
+const res = shortestCompletingWord("1s3 PSt", [
+  "step",
+  "steps",
+  "stripe",
+  "stepple",
+]);
+
+res;
